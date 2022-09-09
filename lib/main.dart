c9cb39bdf7e6eaeb278cb7c8dffb1a4f5dede9a3
onio <astitvastu@gmail.com>
2022-09-09 19:28:03 +0530

    onboarding done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_nord_theme/flutter_nord_theme.dart';
-import 'package:ig/screens/home.dart';
+import 'package:ig/screens/onboarding.dart';
 
 void main() {
   runApp(MaterialApp(
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,196 +1,22 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:entry/entry.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'dart:math' as math;
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<Home> createState() => _HomeState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  TextStyle secHead = GoogleFonts.archivoBlack(
-    color: Colors.white,
-    fontSize: 30,
-    fontWeight: FontWeight.w800,
-  );
-  TextStyle colSecHead = GoogleFonts.archivoBlack(
-    color: Color(0xff2bc9b3),
-    fontSize: 35,
-    fontWeight: FontWeight.w500,
-  );
-  TextStyle priHead = GoogleFonts.archivoBlack(
-      color: Colors.white,
-      fontSize: 40,
-      fontWeight: FontWeight.w800,
-      shadows: <Shadow>[
-        Shadow(color: Color(0xff2bc9b3), offset: Offset(5, 0))
-      ]);
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Container(
+    return Scaffold(
+      body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Image.asset(
-          'assets/images/app_back2.jpg',
-          fit: BoxFit.fill,
-        ),
       ),
-      CarouselSlider(
-        options: CarouselOptions(
-          disableCenter: false,
-          viewportFraction: 0.9,
-          enableInfiniteScroll: false,
-          height: 900,
-          aspectRatio: 1.2,
-          enlargeCenterPage: true,
-          scrollDirection: Axis.vertical,
-          autoPlay: false,
-        ),
-        items: [
-          Stack(children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 100, 10, 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "IDEATE",
-                    style: priHead,
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "INNOVATE",
-                    style: priHead,
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "INCUBATE",
-                    style: priHead,
-                    textAlign: TextAlign.left,
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(120, 50, 20, 0),
-                  child: Image.asset(
-                    'assets/images/first.png',
-                    fit: BoxFit.fill,
-                    width: 500,
-                    height: 500,
-                  )),
-            )
-          ]),
-          Stack(children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 250, 130, 0),
-                  child: Transform.rotate(
-                    angle: math.pi / 12,
-                    child: Image.asset(
-                      'assets/images/second.png',
-                      width: 400,
-                      height: 320,
-                      fit: BoxFit.fill,
-                    ),
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 100, 10, 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "AI, ML, ROBOTICS,",
-                    style: secHead,
-                    textAlign: TextAlign.right,
-                  ),
-                  Text(
-                    "HACKING, DESIGN, ",
-                    style: secHead,
-                    textAlign: TextAlign.right,
-                  ),
-                  Text(
-                    "ETC.",
-                    style: secHead,
-                    textAlign: TextAlign.right,
-                  ),
-                  Text(
-                    "YOU NAME IT",
-                    style: secHead,
-                    textAlign: TextAlign.right,
-                  ),
-                  Text(
-                    "WE HAVE AN",
-                    style: secHead,
-                    textAlign: TextAlign.right,
-                  ),
-                  Text(
-                    "EVENT",
-                    style: colSecHead,
-                    textAlign: TextAlign.right,
-                  ),
-                ],
-              ),
-            ),
-          ]),
-          Stack(children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                  padding: const EdgeInsets.fromLTRB(100, 200, 10, 0),
-                  child: Image.asset(
-                    'assets/images/third.png',
-                    width: 450,
-                    height: 250,
-                    fit: BoxFit.fill,
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 100, 10, 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "WE HAVE A VAST",
-                    style: secHead,
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "COLLECTION OF",
-                    style: secHead,
-                    textAlign: TextAlign.left,
-                  ),
-                  Text(
-                    "CONTENT",
-                    style: colSecHead,
-                    textAlign: TextAlign.left,
-                  ),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                "EXPLORE NOW",
-                style: secHead,
-                textAlign: TextAlign.left,
-              ),
-            ),
-          ]),
-        ],
-      ),
-    ]);
+    );
   }
 }
+
+
